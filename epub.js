d117153815cf617dfc2a5c72b5634d285aa6136c
Christian Roy <roychristian@gmail.com>
2019-07-12 13:35:40 -0400

    Fix "Uncaught TypeError: linkparts.shift is not a function" error (#41)
    * Fix crash on empty chapters
    
    * Fix crash on some links
    
    * Fix for empty chapters

--- epub.js
+++ epub.js
@@ -666,8 +666,8 @@ class EPub extends EventEmitter {
     
             // replace links
             str = str.replace(/(\shref\s*=\s*["']?)([^"'\s>]*?)(["'\s>])/g, (function (o, a, b, c) {
-                var linkparts = b && b.split("#"),
-                    link = path.concat([(linkparts.shift() || "")]).join("/").trim(),
+                var linkparts = b && b.split("#");
+                var link = linkparts.length ? path.concat([(linkparts.shift() || "")]).join("/").trim() : '',
                     element;
     
                 for (i = 0, len = keys.length; i < len; i++) {
@@ -718,7 +718,11 @@ class EPub extends EventEmitter {
                     return;
                 }
     
-                var str = data.toString("utf-8");
+                var str = "";
+                if (data) {
+                  str = data.toString("utf-8");
+                };
+
     
                 callback(null, str);
     
