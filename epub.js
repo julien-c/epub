f442da3312deb86248b9d96eb3fa19e902197674
chenkun <chenkun@shxrtech.com>
2020-07-26 10:06:48 +0800

    获取封面的第二种方式

--- epub.js
+++ epub.js
@@ -753,7 +753,13 @@ class EPub extends EventEmitter {
 
             this.getFile(id, callback);
         } else {
-            callback(new Error("File not found"));
+            const coverId = Object.keys(this.manifest).find(key => (
+              this.manifest[key].properties === 'cover-image'))
+            if (coverId) {
+              this.getFile(coverId, callback)
+            } else {
+              callback(new Error('File not found'))
+            }
         }
     };
 
